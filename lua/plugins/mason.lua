3855770fd38e59b957dd1449da66e46a61415928
cwilson58 <wilsoncameron42@gmail.com>
2024-01-29 11:43:28 -0600

    Add autocomplete via lua_snip and LSP snippets

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,29 +1,39 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-       "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls","csharp_ls","clangd","tsserver"}
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.csharp_ls.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.tsserver.setup({})
-            vim.keymap.set('n','K',vim.lsp.buf.hover,{})
-            vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
-            vim.keymap.set({'n','v'},"<leader>ca",vim.lsp.buf.code_action,{})
-        end
-    }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "tsserver" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
+
